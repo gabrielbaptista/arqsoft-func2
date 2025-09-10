2ebdedfb03ec5bab63daded57ca3532eb8092edf
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2025-09-10 20:25:13 -0300

    Ajuste de segurança

--- FunctionHello.cs
+++ FunctionHello.cs
@@ -15,7 +15,7 @@ namespace ec9
         }
 
         [Function("FunctionHello")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
